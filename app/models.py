37537e1a8f4fa7e66514733c7498bbf8eeb8b9f3
root <root@TATY-LAPTOP.localdomain>
2026-07-09 23:15:54 +0100

    Añadir database.py y models.py con modelo Product

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from app.database import Base
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    price = Column(Float, nullable=False)
+    in_stock = Column(Boolean, default=True)

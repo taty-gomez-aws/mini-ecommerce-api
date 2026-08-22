b0ffad01ec1a40df16e699c684e62417f8dfe722
jgomez <jgomez.aws@gmail.com>
2026-08-22 18:43:03 +0100

    Added comments

--- app/database.py
+++ app/database.py
@@ -3,12 +3,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./ecommerce.db"
 
+# engine
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+# local session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# base
 Base = declarative_base()
 
 
